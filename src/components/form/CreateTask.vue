9322dfff46fb874b4dac5ad46c05311c27f589b0
Mikhail Levchenko <mishalevchenko161@gmail.com>
2024-10-02 00:26:38 +0300

    Начал исправлять форму создания задачи

--- src/components/form/CreateTask.vue
+++ src/components/form/CreateTask.vue
@@ -1,318 +1,294 @@
 <script setup lang="ts">
-import { h, ref } from 'vue'
-import * as z from 'zod'
+import { computed, h, ref } from 'vue'
+import { FieldArray, useForm } from 'vee-validate'
 import { toTypedSchema } from '@vee-validate/zod'
 import { Check, ChevronsUpDown, Plus } from 'lucide-vue-next'
-import { CalendarDate, DateFormatter, getLocalTimeZone, today, type DateValue } from '@internationalized/date'
+import * as z from 'zod'
+import { Cross1Icon } from '@radix-icons/vue'
 import { cn } from '@/lib/utils'
-import router from '@/router'
-import {
-  toast
-} from 'vue-sonner'
 
-import RadixIconsCalendar from '@radix-icons/vue/CalendarIcon'
-import {
-	toDate
-} from 'radix-vue/date'
-import {
-	Form,
-	FormControl,
-	FormDescription,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage
-} from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import { CalendarDate, DateFormatter, getLocalTimeZone, today, type DateValue } from '@internationalized/date'
+import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Separator } from '@/components/ui/separator'
 import { Textarea } from '@/components/ui/textarea'
-import {
-	Command,
-	CommandEmpty,
-	CommandGroup,
-	CommandInput,
-	CommandItem,
-	CommandList,
-} from '@/components/ui/command'
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
-import { Button } from '@/components/ui/button'
-import {
-	Popover,
-	PopoverContent,
-	PopoverTrigger,
-} from '@/components/ui/popover'
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import { Calendar } from '@/components/ui/calendar'
+import RadixIconsCalendar from '@radix-icons/vue/CalendarIcon'
 
-
-import {useListStore} from '@/store/ListsStore'
+import { useListStore } from '@/store/ListsStore'
 import { useGlobalStore } from '@/store/GlobalStore'
 import { useUserStore } from '@/store/UserStore'
-import { emit } from 'process'
 
-
-interface Props {
-	listId?: string
-}
-
-const props = defineProps<Props>()
-
-//Сторы
 const globalStore = useGlobalStore()
 const userStore = useUserStore()
 const listStore = useListStore()
 
+import { ComboboxAnchor, ComboboxContent, ComboboxInput, ComboboxPortal, ComboboxRoot } from 'radix-vue'
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from '@/components/ui/command'
+import { TagsInput, TagsInputInput, TagsInputItem, TagsInputItemDelete, TagsInputItemText } from '@/components/ui/tags-input'
+
+const frameworks = [
+{ value: 'zimin@mail.ru', label: 'Антон Зимин' },
+{ value: 'levchenko@mail.ru', label: 'Михаил Левченко' },
+{ value: 'moiseev@mail.ru', label: 'Сергей Моисеев' },
+{ value: 'kurbatova@mail.ru', label: 'Настя Курбатова' },
+]
+
+const modelValue = ref<string[]>([])
 const open = ref(false)
-const openDate = ref(false)
-const dateValue = ref()
-const placeholder = ref()
-const value = ref('')
+const openUserList = ref(false)
+const searchTerm = ref('')
 
-type User = (typeof users.value)[number]
-const openUserSelect = ref(false)
-const selectedUsers = ref<User[]>([])
-const list = ref(listStore.list)
+const filteredFrameworks = computed(() => frameworks.filter(i => !modelValue.value.includes(i.value)))
 
 
-const currList = ref(listStore.getItemById(props.listId))
 
-// console.log(currList.value);
+import { Calendar } from '@/components/ui/calendar'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import { Button } from '@/components/ui/button'
+import { toast } from '@/components/ui/toast'
+import {
+  toDate
+} from 'radix-vue/date'
 
 
 const types = [
-	{ label: 'Высокий', value: 'high' },
-	{ label: 'Средний', value: 'medium' },
-	{ label: 'Низкий', value: 'low' },
-] as const
+{ label: 'Высокий', value: 'high' },
+{ label: 'Средний', value: 'medium' },
+{ label: 'Низкий', value: 'low' },
+]
+
+const users = [
+{ value: 'zimin@mail.ru', label: 'Антон Зимин' },
+{ value: 'levchenko@mail.ru', label: 'Михаил Левченко' },
+{ value: 'moiseev@mail.ru', label: 'Сергей Моисеев' },
+{ value: 'kurbatova@mail.ru', label: 'Настя Курбатова' },
+]
+
+const dateValue = ref()
+const placeholder = ref()
+const openDate = ref(false)
 
 const df = new DateFormatter('ru-RU', {
-	dateStyle: 'long',
+  dateStyle: 'long',
 })
-const formResult = {
-	id: currList.value.tasks.length + 1,
-	title: '',
-	status: 'todo',
-	label: null,
-	priority: '',
-	description: '',
-	deadLine: dateValue,
-	participants: [],
-	messages: [],
-	responsible:{}
-}
-const accountFormSchema = toTypedSchema(z.object({
-	name: z
-		.string({
-			required_error: 'Обязательное поле.',
-		}),
-	description: z
-		.string({
-			required_error: 'Обязательное поле.',
-		}),
-	date: z.string().datetime().optional().refine(date => date !== undefined, 'Выберите дату.'),
-	priority: z
-		.string({
-			required_error: 'Обязательное поле.',
-		})
-		.min(1, 'Необходимо выбрать тип.'),
-	responsible: z
-		.string({
-			required_error: 'Обязательное поле.',
-		})
-		.min(1, 'Необходимо выбрать тип.'),
+
+const profileFormSchema = toTypedSchema(z.object({
+  title: z
+  .string(
+  {
+    message: 'Обязательное поле',
+  }
+  )
+  .min(2, {
+    message: 'Минимум 2 символа',
+  })
+  .max(30, {
+    message: 'Username must not be longer than 30 characters.',
+  }),
+  description: z.string({
+    message: 'Обязательное поле',
+  }).max(200, { message: 'Максимум 200 символов' }).min(4, { message: 'Минимум 2 символа.' }),
+  lastUpdate: z.string().datetime().optional().refine(date => date !== undefined, 'Выберите дату.'),
+  priority: z
+  .string({
+    required_error: 'Обязательное поле.',
+  })
+  .min(1, 'Необходимо выбрать тип.'),
+  participant: z
+  .string({
+    required_error: 'Обязательное поле.',
+  })
+  .min(1, 'Необходимо исполнителя.'),
 }))
 
-function getImageUrl(name) {
-	return new URL(`../../assets/avatars/${name}`, import.meta.url).href
+const { handleSubmit, setFieldValue, resetForm } = useForm({
+  validationSchema: profileFormSchema,
+})
+
+const reset = () => {
+  resetForm()
+  modelValue.value = []
 }
-
-
-const url = window.location.href;
-const lastParam = url.split("/").slice(-1)[0];
-
-const options = {
-	day: 'numeric',
-	month: 'long',
-};
-
-const emit = defineEmits(['close'])
-function onSubmit(values) {
-  listStore.addTask(formResult, lastParam)
-  emit('close')
-	globalStore.isSheetOpen = false
-	toast("Лист успешно создан");
-}
-
+const onSubmit = handleSubmit((values) => {
+  console.log(values);
+  // listStore.addList(values)
+  // toast('Лист успешно создан');
+})
 </script>
 
 <template>
-	<Form v-slot="{ setFieldValue }" :validation-schema="accountFormSchema" class="space-y-6 mt-3 flex flex-col max-h-[95%]"
-		@submit="onSubmit">
-		<div class="form__fields max-h-[350px] overflow-auto md:overflow-auto space-y-4 md:space-y-6 md:max-h-none">
-			<FormField v-slot="{ componentField }" name="name">
-				<FormItem>
-					<FormLabel>Название задачи</FormLabel>
-					<FormControl>
-						<Input type="text" placeholder="Введите название" v-bind="componentField"
-							v-model="formResult.title" />
-					</FormControl>
+  <form class="space-y-8 mt-4" @submit="onSubmit">
+    <FormField v-slot="{ componentField }" name="title">
+      <FormItem>
+        <FormLabel>Название</FormLabel>
+        <FormControl>
+          <Input type="text" placeholder="Новый лист" v-bind="componentField" />
+        </FormControl>
+        <FormMessage />
+      </FormItem>
+    </FormField>
 
-					<FormDescription>
-						Название будет отображаться в спике задач. По названию можно легко найти задачу в поиске
-					</FormDescription>
-				</FormItem>
-			</FormField>
-			<FormField v-slot="{ componentField }" name="description">
-				<FormItem>
-					<FormLabel>Описание задачи</FormLabel>
-					<FormControl>
-						<Textarea placeholder="Введите описание" v-bind="componentField"
-							v-model="formResult.description" />
-					</FormControl>
+    <FormField v-slot="{ componentField }" name="description">
+      <FormItem>
+        <FormLabel>Описание листа</FormLabel>
+        <FormControl>
+          <Textarea placeholder="Краткое (или нет) описание листа" v-bind="componentField" />
+        </FormControl>
+        <FormMessage />
+      </FormItem>
+    </FormField>
 
-					<FormDescription>
-						Описание будет распологаться под названием и должно содержать краткую информацию о листе
-					</FormDescription>
-				</FormItem>
-			</FormField>
-			<FormField v-slot="{ field, value }" name="date">
-				<FormItem class="flex flex-col">
-					<FormLabel>Дедлайн</FormLabel>
-					<Popover v-model:open="openDate">
-						<PopoverTrigger as-child>
-							<FormControl>
-								<Button variant="outline" :class="cn(
-									'w-[240px] justify-start text-left font-normal',
-									!value && 'text-muted-foreground',
-								)">
-									<RadixIconsCalendar class="mr-2 h-4 w-4 opacity-50" />
-									<span>{{ value ? df.format(toDate(dateValue, getLocalTimeZone())) : "Выберите день"
-										}}</span>
-								</Button>
-							</FormControl>
-						</PopoverTrigger>
-						<PopoverContent class="p-0">
-							<Calendar v-model:placeholder="placeholder" v-model="dateValue"
-								calendar-label="День окончания" initial-focus :min-value="today(getLocalTimeZone())"
-								@update:model-value="(v) => {
-									if (v) {
-										dateValue = v
-										openDate = false
-										formResult.deadLine = toDate(v).toLocaleDateString('ru-RU', options)
-										setFieldValue('date', toDate(v).toISOString())
-									}
-									else {
-										dateValue = undefined
-										setFieldValue('date', undefined)
-									}
-									}" />
-						</PopoverContent>
-					</Popover>
-					<FormDescription>
-						Дата, до которой должна быть выполнена задача
-					</FormDescription>
-				</FormItem>
-				<input type="hidden" v-bind="field">
-			</FormField>
-			<FormField v-slot="{ value }" name="priority">
-				<FormItem class="flex flex-col">
-					<FormLabel>Приоритет задачи</FormLabel>
+    <FormField v-slot="{ field, value }" name="lastUpdate">
+      <FormItem class="flex flex-col">
+        <FormLabel>Дата окончания</FormLabel>
+        <Popover v-model:open="openDate">
+          <PopoverTrigger as-child>
+            <FormControl>
+              <Button variant="outline" :class="cn(
+              'w-[240px] justify-start text-left font-normal',
+              !value && 'text-muted-foreground',
+              )">
+              <RadixIconsCalendar class="mr-2 h-4 w-4 opacity-50" />
+              <span>{{ value ? df.format(toDate(dateValue, getLocalTimeZone())) : "Выберите день"
+              }}</span>
+            </Button>
+          </FormControl>
+        </PopoverTrigger>
+        <PopoverContent class="p-0">
+          <Calendar v-model:placeholder="placeholder" v-model="dateValue" calendar-label="День окончания" initial-focus
+          :min-value="today(getLocalTimeZone())" @update:model-value="(v) => {
+            if (v) {
+              dateValue = v
+              openDate = false
+              setFieldValue('lastUpdate', toDate(v).toISOString())
+              console.log(toDate(v).toISOString());
+            }
+            else {
+              dateValue = undefined
+              setFieldValue('lastUpdate', undefined)
+            }
+          }" />
+        </PopoverContent>
+      </Popover>
+      <FormDescription>
+        День окончания всех внутренних задач
+      </FormDescription>
+    </FormItem>
+    <input type="hidden" v-bind="field">
+  </FormField>
+  <FormField v-slot="{ value }" name="priority">
+    <FormItem class="flex flex-col">
+      <FormLabel>Приоритет задачи</FormLabel>
 
-					<Popover v-model:open="open">
-						<PopoverTrigger as-child>
-							<FormControl>
-								<Button variant="outline" role="combobox" :aria-expanded="open" :class="cn(
-						'w-[200px] justify-between',
-						!value && 'text-muted-foreground',
-					)">
-									{{ value ? types.find(
-									(type) => type.value === value,
-									)?.label : 'Выберите приоритет' }}
+      <Popover v-model:open="open">
+        <PopoverTrigger as-child>
+          <FormControl>
+            <Button variant="outline" role="combobox" :aria-expanded="open" :class="cn(
+            'w-[200px] justify-between',
+            !value && 'text-muted-foreground',
+            )">
+            {{ value ? types.find(
+              (type) => type.value === value,
+              )?.label : 'Выберите приоритет' }}
 
-									<ChevronsUpDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
-								</Button>
-							</FormControl>
-						</PopoverTrigger>
-						<PopoverContent class="w-[200px] p-0">
-							<Command>
-								<CommandList>
-									<CommandGroup>
-										<CommandItem v-for="type in types" :key="type.value" :value="type.label"
-											@select="() => {
-												formResult.priority = type.value
-												setFieldValue('priority', type.value)
-												open = false
-											}">
-											<Check :class="cn(
-												'mr-2 h-4 w-4',
-												value === type.value ? 'opacity-100' : 'opacity-0',
-												)" />
-											{{ type.label }}
-										</CommandItem>
-									</CommandGroup>
-								</CommandList>
-							</Command>
-						</PopoverContent>
-					</Popover>
+              <ChevronsUpDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
+            </Button>
+          </FormControl>
+        </PopoverTrigger>
+        <PopoverContent class="w-[200px] p-0">
+          <Command>
+            <CommandList>
+              <CommandGroup>
+                <CommandItem v-for="type in types" :key="type.value" :value="type.label"
+                @select="() => {
+                  setFieldValue('priority', type.value)
+                  open = false
+                }">
+                <Check :class="cn(
+                'mr-2 h-4 w-4',
+                value === type.value ? 'opacity-100' : 'opacity-0',
+                )" />
+                {{ type.label }}
+              </CommandItem>
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
 
 
-					<FormDescription>
+    <FormDescription>
 
-					</FormDescription>
-				</FormItem>
-			</FormField>
-			<FormField  v-slot="{ field, value }" name="responsible">
-				<FormItem class="flex flex-col">
-					<FormLabel>Исполнитель</FormLabel>
+    </FormDescription>
+  </FormItem>
+</FormField>
+<FormField v-slot="{ value }" name="participant">
+  <FormItem class="flex flex-col">
+    <FormLabel>Исполнитель</FormLabel>
 
-						<Select v-bind="field"
-						@update:model-value="(v) => {
-							if (v) {
-								console.log(v);
-								formResult.responsible = globalStore.defaultUsers.find(person => person.email === v)
-							}
-						}">
-							<SelectTrigger
-							id="model"
-							class="items-center [&_[data-description]]:hidden h-auto"
-							>
-								<SelectValue placeholder="Выберите исполнителя" class="flex items-center" />
-							</SelectTrigger>
-							<SelectContent>
-								<SelectItem v-for="person in globalStore.defaultUsers" :value=person.email>
-									<div class="flex items-center gap-3 text-muted-foreground">
-										<Avatar>
-											<AvatarImage :src="getImageUrl(person.avatar)" alt="Image" />
-											<AvatarFallback>{{ person.name[0] }}</AvatarFallback>
-										</Avatar>
-										<div class="grid gap-0.5">
-											<p>
-												{{ person.surname }}
-												<span class="font-medium text-foreground">
-													{{ person.name }}
-												</span>
-											</p>
-											<p class="text-xs" data-description>
-												{{ person.email }}
-											</p>
-										</div>
-									</div>
-								</SelectItem>
-							</SelectContent>
-						</Select>
-					<FormDescription>
-						Выберите ответственного для задачи человека
-					</FormDescription>
-				</FormItem>
-			</FormField>
-		</div>
-		<div class="flex justify-start gap-2 mt-auto">
-			<Button type="submit" class="outline-0 flex-auto md:flex-none">
-				Создать лист
-			</Button>
-			<!-- <Button class="bg-transparent text-primary border hover:bg-primary-foreground outline-0">
-				Сбросить значение полей
-			</Button> -->
-		</div>
-	</Form>
+    <Popover v-model:open="openUserList">
+      <PopoverTrigger as-child>
+        <FormControl>
+          <Button variant="outline" role="combobox" :aria-expanded="open" :class="cn(
+          'w-[200px] justify-between',
+          !value && 'text-muted-foreground',
+          )">
+          {{ value ? users.find(
+            (type) => type.value === value,
+            )?.label : 'Выберите приоритет' }}
+
+            <ChevronsUpDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </FormControl>
+      </PopoverTrigger>
+      <PopoverContent class="w-[200px] p-0">
+        <Command>
+          <CommandList>
+            <CommandGroup>
+              <CommandItem v-for="type in users" :key="type.value" :value="type.label"
+              @select="() => {
+                setFieldValue('participant', type.value)
+                openUserList = false
+              }">
+              <Check :class="cn(
+              'mr-2 h-4 w-4',
+              value === type.value ? 'opacity-100' : 'opacity-0',
+              )" />
+              {{ type.label }}
+            </CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </Command>
+    </PopoverContent>
+  </Popover>
+
+
+  <FormDescription>
+
+  </FormDescription>
+</FormItem>
+</FormField>
+
+
+<div class="flex gap-2 justify-start">
+  <Button type="submit">
+    Создать задачу
+  </Button>
+
+  <Button type="button" variant="outline" @click="reset">
+    Сбросить значения
+  </Button>
+</div>
+</form>
 </template>
